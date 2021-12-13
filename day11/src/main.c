5b518088cd91f9e77e9842f63cbf8791e72c0701
MoltenCoffee <13321277+MoltenCoffee@users.noreply.github.com>
2021-12-13 15:25:35 +0100

    Day 11 part 1

---
+++ day11/src/main.c
@@ -0,0 +1,118 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../lib/common.h"
+
+#define ROW_COUNT 10
+#define COL_COUNT 10
+
+typedef uint8_t Grid[ROW_COUNT][COL_COUNT];
+
+static void propagateFlash(Grid rows, Grid flashed) {
+  // Flash all octopuses with 9 or higher
+  bool loopAgain = false;
+  for (int i = 0; i < ROW_COUNT; i++) {
+    for (int j = 0; j < COL_COUNT; j++) {
+      if (rows[i][j] > 9 && flashed[i][j] != 1) {
+        flashed[i][j] = 1;
+        loopAgain = true;
+        // And increase all surrounding
+        // above
+        if (i > 0) rows[i - 1][j]++;
+        // below
+        if (i < ROW_COUNT - 1) rows[i + 1][j]++;
+        // left
+        if (j > 0) rows[i][j - 1]++;
+        // right
+        if (j < COL_COUNT - 1) rows[i][j + 1]++;
+
+        // above left
+        if (i > 0 && j > 0) rows[i - 1][j - 1]++;
+        // above right
+        if (i > 0 && j < COL_COUNT - 1) rows[i - 1][j + 1]++;
+        // below left
+        if (i < ROW_COUNT - 1 && j > 0) rows[i + 1][j - 1]++;
+        // below right
+        if (i < ROW_COUNT - 1 && j < COL_COUNT - 1) rows[i + 1][j + 1]++;
+      }
+    }
+  }
+  if (loopAgain) propagateFlash(rows, flashed);
+}
+
+static uint8_t step(Grid rows) {
+  Grid flashed = {0};
+
+  // Increase all by one
+  for (int i = 0; i < ROW_COUNT; i++) {
+    for (int j = 0; j < COL_COUNT; j++) {
+      rows[i][j]++;
+    }
+  }
+
+  propagateFlash(rows, flashed);
+
+  // Reset all that flashed, count flashed
+  uint8_t flashCount = 0;
+  for (int i = 0; i < ROW_COUNT; i++) {
+    for (int j = 0; j < COL_COUNT; j++) {
+      if (flashed[i][j] == 1) {
+        rows[i][j] = 0;
+        flashCount++;
+      }
+    }
+  }
+  return flashCount;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "\033[1;31mExactly one argument expected: path to input\n");
+    return EXIT_FAILURE;
+  }
+  char* file = readFile(argv[1]);
+
+  Grid rows = {0};
+
+  uint8_t charCount = 0;
+  uint8_t lineCount = 0;
+  uint8_t colCount = 0;
+  char prev;
+  for (;;) {
+    char c = file[charCount];
+    charCount++;
+
+    if (isdigit(c)) {
+      rows[lineCount][colCount] = (uint8_t)(c - 48);
+      colCount++;
+    } else if (c == '\n') {
+      if (prev == '\n') continue;
+      lineCount++;
+      colCount = 0;
+    } else if (c == '\0') {
+      break;
+    }
+    prev = c;
+  }
+  free(file);
+
+  for (int i = 0; i < ROW_COUNT; i++) {
+    for (int j = 0; j < COL_COUNT; j++) {
+      printf("%d", rows[i][j]);
+    }
+    printf("%c", '\n');
+  }
+
+  // Model steps
+  uint16_t flashCount = 0;
+  for (int i = 0; i < 100; i++) {
+    flashCount += step(rows);
+  }
+
+  printf("Part 1: Total flashed after 100 steps: %d\n", flashCount);
+
+  return EXIT_SUCCESS;
+}
